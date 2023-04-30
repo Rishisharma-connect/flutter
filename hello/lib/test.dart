8e145848e6e71407233e6cd10f52695ea05a2701
Rishi Sharma <rishisharma.connect@gmail.com>
2023-04-30 12:32:16 +0100

    fluuer code

---
+++ hello/lib/test.dart
@@ -0,0 +1,41 @@
+void main() {
+  int p = 6;
+
+  int q = 5;
+
+// Greater between p and q
+
+  var r = p > q;
+
+  print("p is greater than q is $r");
+
+// Smaller between p and q
+
+  var s = p < q;
+
+  print("p is smaller than q is $s");
+
+// Greater than or equal to
+
+  var t = p >= q;
+
+  print("p is greater than or equal to q is $t");
+
+// Less than or equal to
+
+  var u = p <= q;
+
+  print("p is smaller than or equal to q is $u");
+
+// Equality between p and q
+
+  var v = p == q;
+
+  print("p and q are equal is $v");
+
+// Unequality between p and q
+
+  var w = p != q;
+
+  print("p and q are not equal is $w");
+}
